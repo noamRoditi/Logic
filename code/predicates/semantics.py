49adca827b22377cced93bbec09598098c2fb16c
RonLavi1 <laviron93@gmail.com>
2018-11-29 22:14:03 +0200

    passing 1-8

--- code/predicates/semantics.py
+++ code/predicates/semantics.py
@@ -44,6 +44,13 @@ class Model:
             get their value from the given assignment """
         assert term.variables().issubset(assignment.keys())
         # Task 7.7
+        if is_constant(term.root):
+            return self.meaning[term.root]
+        if is_variable(term.root):
+            return assignment[term.root]
+        if is_function(term.root):
+            values = tuple([Model.evaluate_term(self, argument, assignment) for argument in term.arguments])
+            return self.meaning[term.root].get(values)
 
     def evaluate_formula(self, formula, assignment={}):
         """ Return the value of the given formula in this model, where
@@ -51,6 +58,42 @@ class Model:
             from the given assignment """
         assert formula.free_variables().issubset(assignment.keys())
         # Task 7.8
+        if is_unary(formula.root):
+            return not Model.evaluate_formula(self, formula.first, assignment)
+        if is_binary(formula.root):
+            return self.formula_evaluation_is_binary(formula,assignment)
+        if is_equality(formula.root):
+            return Model.evaluate_term(self, formula.first, assignment) is Model.evaluate_term(self, formula.second, assignment)
+        if is_relation(formula.root):
+            argument = tuple([Model.evaluate_term(self, argument, assignment) for argument in formula.arguments])
+            return argument in self.meaning[formula.root]
+        if is_quantifier(formula.root):
+            return self.formula_evaluation_is_quantifier(formula, assignment)
+
+    def formula_evaluation_is_binary(self, formula, assignment):
+        if formula.root == '&':
+            return Model.evaluate_formula(self, formula.first, assignment) and \
+                   Model.evaluate_formula(self, formula.second, assignment)
+        if formula.root == '|':
+            return Model.evaluate_formula(self, formula.first, assignment) or \
+                   Model.evaluate_formula(self, formula.second, assignment)
+        return Model.evaluate_formula(self, formula.first, assignment) or not \
+            Model.evaluate_formula(self, formula.second, assignment)
+
+    def formula_evaluation_is_quantifier(self,formula, assignment):
+        if formula.root == 'A':
+            for element in self.universe:
+                assignment[formula.variable] = element
+                if Model.evaluate_formula(self, formula.predicate, assignment):
+                    continue
+                return False
+            return True
+        if formula.root == 'E':
+            for element in self.universe:
+                assignment[formula.variable] = element
+                if Model.evaluate_formula(self, formula.predicate, assignment):
+                    return True
+            return False
 
     def is_model_of(self, formulae):
         """ Return whether self is a model of the given formulae. For this to be
--- code/predicates/syntax.py
+++ code/predicates/syntax.py
@@ -2,11 +2,10 @@
     Mathematical Logic through Programming
     by Gonczarowski and Nisan.
     File name: code/predicates/syntax.py """
-import re
 
 from propositions.syntax import Formula as PropositionalFormula, \
-                                is_variable as is_propositional_variable
-# from predicates.util import *
+    is_variable as is_propositional_variable
+
 
 class ForbiddenVariableError(Exception):
     """ Raised when a substituted expression contains a free variable that is
@@ -17,18 +16,22 @@ class ForbiddenVariableError(Exception):
         assert is_variable(variable_name)
         self.variable_name = variable_name
 
+
 def is_variable(s):
     """ Is s a variable name? """
     return s[0] >= 'u' and s[0] <= 'z' and s.isalnum()
 
+
 def is_constant(s):
     """ Is s a constant name? """
-    return  ((s[0] >= '0' and s[0] <= '9') or (s[0] >= 'a' and s[0] <= 'd')) \
-            and s.isalnum()
+    return ((s[0] >= '0' and s[0] <= '9') or (s[0] >= 'a' and s[0] <= 'd')) \
+           and s.isalnum()
+
 
 def is_function(s):
     """ Is s a function name? """
     return s[0] >= 'f' and s[0] <= 't' and s.isalnum()
+
 
 class Term:
     """ A term in a first-order logical formula, composed from variable names
@@ -48,7 +51,7 @@ class Term:
 
     def __eq__(self, other):
         return type(other) is Term and str(self) == str(other)
-        
+
     def __ne__(self, other):
         return not self == other
 
@@ -61,11 +64,10 @@ class Term:
         if is_variable(self.root) or is_constant(self.root):
             return self.root
         else:
-            result = self.root+'('
+            result = self.root + '('
             for arg in self.arguments:
                 result = result + str(arg) + ','
             return result[:len(result) - 1] + ')'
-
 
     @staticmethod
     def parse_prefix(s):
@@ -73,49 +75,37 @@ class Term:
             parsed term, and the unparsed remainder of the string """
         assert type(s) is str
         # Task 7.3.1
-        if is_variable(s[0]) or is_constant(s[0]):  # if string starts with variable or constant
-            regex = re.compile('([u-z]|[a-d]|[0-9])[0-9]*')
-            end_of_var = regex.match(s).end()
-            return Term(s[:end_of_var]), s[end_of_var:]
-        # term is a function look for the end of brackets
-        start_of_brackets = s.find('(')
-        if start_of_brackets == -1:
-            return None, 'invalid term'
-        end_of_brackets = find_closing_bracket_index(s, start_of_brackets)
-        if end_of_brackets == start_of_brackets + 1:
-            return None, 'invalid brackets'
-        arguments = Term.prase_arguments(s[start_of_brackets + 1: end_of_brackets])
-        if arguments is None:
-            return None, 'invalid term arguments'
-        return Term(s[:start_of_brackets],arguments), s[end_of_brackets + 1:]
-
-
-    @staticmethod
-    def prase_arguments(s):
-        """ helper function to parse a list of terms"""
-        terms = s.split(",")
-        parsed_terms = []
-        for term in terms:
-            parsed_term = Term.parse_prefix(term)
-            if parsed_term[1] != '':
-                return None
-            parsed_terms.append(parsed_term[0])
-        return parsed_terms
+        if is_constant(s[0]) or is_variable(s[0]):
+            for i in range(1, len(s) + 1):
+                if is_constant(s[:i]) or is_variable(s[:i]):
+                    continue
+                return Term(s[:i - 1]), s[i - 1:]
+            return Term(s), ""
+        if is_function(s[0]):
+            remainder = s[s.find('(') + 1:]
+            arguments, remainder = Formula.find_reminder_arguments(remainder)
+            fx = s[:s.find('(')]
+            return Term(fx, arguments), remainder[1:]
 
     @staticmethod
     def parse(s):
         """ Return a term parsed from the given string representation """
         assert type(s) is str
         # Task 7.3.2
-        term = Term.parse_prefix(s)
-        if term[1] != '':
-            return None
-        else:
-            return term[0]
+        return Term.parse_prefix(s)[0]
 
     def variables(self):
         """ Return the set of variables in this term """
         # Task 7.5
+        if is_constant(self.root):
+            return set()
+        if is_variable(self.root):
+            return {self.root}
+        if is_function(self.root):
+            result = set()
+            for argument in self.arguments:
+                result = result | argument.variables()
+            return result
 
     def functions(self):
         """ Return a set of pairs (function_name, arity) for all function names
@@ -140,25 +130,31 @@ class Term:
             assert is_variable(variable)
         # Task 9.1
 
+
 def is_equality(s):
     """ Is s the equality relation? """
     return s == '='
+
 
 def is_relation(s):
     """ Is s a relation name? """
     return s[0] >= 'F' and s[0] <= 'T' and s.isalnum()
 
+
 def is_unary(s):
     """ Is s a unary Boolean operator? """
     return s == '~'
+
 
 def is_binary(s):
     """ Is s a binary Boolean operator? """
     return s == '&' or s == '|' or s == '->'
 
+
 def is_quantifier(s):
     """ Is s a quantifier? """
     return s == 'A' or s == 'E'
+
 
 def find_closing_bracket_index(s, start_index, open_bracket="(", closed_bracket=")"):
     """ helper function that given start_index that is an open bracket will return
@@ -175,26 +171,28 @@ def find_closing_bracket_index(s, start_index, open_bracket="(", closed_bracket=
     if s[lastBracketIndex] != closed_bracket or numOfOpenBrackets > 0:
         return None
     return lastBracketIndex
+
+
 class Formula:
     """ A Formula in first-order logic """
-    
+
     def __init__(self, root, first=None, second=None):
-        if is_equality(root): # Populate self.first and self.second
+        if is_equality(root):  # Populate self.first and self.second
             assert type(first) is Term and type(second) is Term
             self.root, self.first, self.second = root, first, second
-        elif is_relation(root): # Populate self.root and self.arguments
+        elif is_relation(root):  # Populate self.root and self.arguments
             assert second is None
             assert type(first) is list
             for argument in first:
                 assert type(argument) is Term
             self.root, self.arguments = root, first
-        elif is_unary(root): # Populate self.first
+        elif is_unary(root):  # Populate self.first
             assert type(first) is Formula and second is None
             self.root, self.first = root, first
-        elif is_binary(root): # Populate self.first and self.second
+        elif is_binary(root):  # Populate self.first and self.second
             assert type(first) is Formula and type(second) is Formula
-            self.root, self.first, self.second = root, first, second           
-        else: # Populate self.variable and self.predicate
+            self.root, self.first, self.second = root, first, second
+        else:  # Populate self.variable and self.predicate
             assert is_quantifier(root) and is_variable(first) and \
                    type(second) is Formula
             self.root, self.variable, self.predicate = root, first, second
@@ -209,7 +207,7 @@ class Formula:
             # if this is a quantifier with a formula inside the [] remove the ()
             predicate = str(self.predicate)
             if predicate[0] == '(':
-                predicate = predicate[1:len(predicate)-1]
+                predicate = predicate[1:len(predicate) - 1]
             return self.root + self.variable + '[' + predicate + ']'
         elif is_relation(self.root):
             result = self.root + '('
@@ -220,14 +218,13 @@ class Formula:
             else:
                 return result + ')'
         else:
-            if is_equality(self.root) or is_equality(s):
+            if is_equality(self.root):
                 return str(self.first) + self.root + str(self.second)
             return '(' + str(self.first) + self.root + str(self.second) + ')'
 
-
     def __eq__(self, other):
         return type(other) is Formula and str(self) == str(other)
-        
+
     def __ne__(self, other):
         return not self == other
 
@@ -241,96 +238,39 @@ class Formula:
             string """
         assert type(s) is str
         # Task 7.4.1
-        if len(s) == 0:
-            return None, ''
-        if s[0] == ")" or s[0] == "]":
-            return None, 'Unexpected symbol' + s[0]
-        elif is_variable(s[0]) or is_constant(s[0]):
-            var_string_regex = '([u-z]|[a-d]|[0-9])[0-9]*'
-            var_regex = re.compile(var_string_regex)
-            end_of_var = var_regex.match(s).end()
-            formula_regex = re.compile(var_string_regex + '([&|=]|->)' + '(' + var_string_regex + '|' + \
-                            '([F-T]|[f-t])([a-z]|[0-9])*\(\w*\)' + '|' + '([AE][u-z])[0-9]*\[\w*\]' + ')')
-            end_of_formula = formula_regex.match(s)
-            if end_of_formula is None:
-                return None, 'invalid formula that starts with var ot const'
-            end_of_formula = end_of_formula.end()
-            # if end_of_formula < len(s):
-            #     if is_binary(s[end_of_formula]):
-            #         second = Formula.parse_prefix(s[end_of_formula+1:])
-            #         return Formula(s[end_of_formula],Formula.parse_prefix(s[:end_of_formula])[0],
-            #                        second[0]), second[1]
-            #     elif end_of_formula + 1 < len(s) and is_binary(s[end_of_formula:end_of_formula + 2]):
-            #         second = Formula.parse_prefix(s[end_of_formula + 2:])
-            #         return Formula(s[end_of_formula:end_of_formula + 2], Formula.parse_prefix(s[:end_of_formula])[0],
-            #                        second[0]), second[1]
-            operation_len = 1
-            if len(s) > end_of_var:
-                root = s[end_of_var]
-                if root == "-":
-                    operation_len = 2
-                    root = s[end_of_var + 1: end_of_var + 3]
-            else:
-                return None, "invalid Formula"
-            return Formula(root, Term.parse(s[:end_of_var]), Term.parse(s[end_of_var + operation_len:end_of_formula]))\
-                , s[end_of_formula:]
-        elif is_unary(s[0]):
-            if len(s) == 1:
-                return None, 'A formula should follow after ~'
-            x = Formula.parse_prefix(s[1:])
-            if x[0] is None:
-                return None, 'A valid formula should follow after ~'
-            return Formula(s[0], x[0], None), s[len(str(x[0]))+1:]
-        elif is_quantifier(s[0]):
-            # see if variable and [ comes after the quantifier
-            regex = re.compile('([AE][u-z])[0-9]*\[')
-            quantifier_match = regex.match(s)
-            if quantifier_match is None:
-                return None, 'Not a valid quantifier structure'
-            # find the end of the quantifier
-            start_of_square_bracket = quantifier_match.end()
-            end_of_square_bracket = find_closing_bracket_index(s, start_of_square_bracket, "[", "]")
-            if s[end_of_square_bracket] != "]":
-                return None, 'invalid square brackets'
-            reminder = s[end_of_square_bracket + 1:]
-            variable = s[1:start_of_square_bracket - 1]
-            # replace the [] with () so the predicate will be parsed as one
-            predicate = s[start_of_square_bracket:end_of_square_bracket]
-            return Formula(s[0],variable,Formula.parse_prefix(predicate)[0]), reminder
-        elif is_relation(s[0]) or is_function(s[0]):
-            regex = re.compile('([F-T]|[f-t])([a-z]|[0-9])*\(')
-            start_of_relation = regex.match(s).end()
-            end_of_relation = find_closing_bracket_index(s,start_of_relation)
-            if end_of_relation is None:
-                return None, 'invalid relation brackets'
-            terms = s[start_of_relation:end_of_relation].split(",")
-            parsed_terms = []
-            for term in terms:
-                parsed_term = Term.parse(term)
-                if parsed_term is None:
-                    return None, "invalid term inside relation"
-                parsed_terms.append(parsed_term)
-            return Formula(s[:end_of_relation - 2], parsed_terms), s[end_of_relation + 1:]
-        elif s[0] == "(":
-            num_of_open_bracket = 1
-            i = 1
-            last_bracket_index = find_closing_bracket_index(s,1)
-            if last_bracket_index is None:
-                return None, "invalid brackets"
-            while num_of_open_bracket != 0 and last_bracket_index > i:
-                if s[i] == ")":
-                    num_of_open_bracket -= 1
-                elif s[i] == "(":
-                    num_of_open_bracket += 1
-                elif is_binary(s[i]) and num_of_open_bracket == 1:
-                    return Formula(s[i],Formula.parse_prefix(s[1:i])[0],Formula.parse_prefix(s[i+1:last_bracket_index])[0]), \
-                           s[last_bracket_index + 1:]
-                elif i + 1 < len(s) and is_binary(s[i:i+2]) and num_of_open_bracket == 1:
-                    return Formula(s[i:i+2], Formula.parse_prefix(s[1:i]),
-                                   Formula.parse_prefix(s[i + 2:last_bracket_index])), s[last_bracket_index+1:]
-                i += 1
-        else:
-            return None, 'Unfamilar input'
+
+        if is_unary(s[0]):
+            formula, remainder = Formula.parse_prefix(s[1:])
+            return Formula('~', formula), remainder
+        if s[0] is "(":  # binary case
+            left_formula, remainder = Formula.parse_prefix(s[1:])
+            root = remainder[0]
+            right_formula, remainder = Formula.parse_prefix(remainder[len(root):])
+            return Formula(root, left_formula, right_formula), remainder[1:]
+        if is_relation(s[0]):
+            remainder = s[s.find('(') + 1:]
+            relation = s[:s.find('(')]
+            arguments, remainder = Formula.find_reminder_arguments(remainder)
+            return Formula(relation, arguments), remainder[1:]
+        if is_quantifier(s[0]):
+            remainder = s[s.find('['):]
+            variable = s[1:s.find('[')]
+            formula, remainder = Formula.parse_prefix(remainder[1:])
+            return Formula(s[0], variable, formula), remainder[1:]
+        if "=" in s:
+            first, remainder = Term.parse_prefix(s)
+            second, remainder = Term.parse_prefix(remainder[1:])
+            return Formula('=', first, second), remainder
+
+    @staticmethod
+    def find_reminder_arguments(remainder):
+        arguments = []
+        while remainder.index(")") > 0:
+            formula, remainder = Term.parse_prefix(remainder)
+            arguments.append(formula)
+            if remainder[0] == ',':
+                remainder = remainder[1:]
+        return arguments, remainder
 
     @staticmethod
     def parse(s):
@@ -338,10 +278,24 @@ class Formula:
             representation """
         assert type(s) is str
         # Task 7.4.2
+        return Formula.parse_prefix(s)[0]
 
     def free_variables(self):
         """ Return the set of variables that are free in this formula """
         # Task 7.6
+        if is_unary(self.root):
+            return self.first.free_variables()
+        if is_binary(self.root):
+            return self.first.free_variables() | (self.second.free_variables())
+        if is_equality(self.root):
+            return Term.variables(self.first) | (Term.variables(self.second))
+        if is_quantifier(self.root):
+            return self.predicate.free_variables() - set(self.variable)
+        if is_relation(self.root):
+            free_variables = set()
+            for item in self.arguments:
+                free_variables = free_variables | Term.variables(item)
+            return free_variables
 
     def functions(self):
         """ Return a set of pairs (function_name, arity) for all function names
